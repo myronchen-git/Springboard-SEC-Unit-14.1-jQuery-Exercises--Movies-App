89f01233e302b26829876478f6d6250b8b3447d6
Myron Chen <125855279+myronchen-git@users.noreply.github.com>
2024-01-04 14:00:58 -0800

    feat: add movies app JavaScript code
    Added code for adding event listeners on and handlers for the submit
    and remove buttons.
    
    Added code to add and remove movies from the movies database (Map).
    
    Added code for creating and displaying the movies in the HTML.
    
    In the HTML, the unordered list HTML element for listing movies was
    changed to a table, to more easily display and organize the names and
    ratings.
    
    A Map was used to store movie info, because it prevents duplicates.  If
    no data structure was used, then the whole movies table would need to
    be iterated thru for every addition of a movie to check for existence.
    
    In displayMovies(), the line of code to empty out the movies table is
    for a future commit involving sorting.

---
+++ script.js
@@ -0,0 +1,80 @@
+$(function () {
+  new MoviesApp();
+});
+
+class MoviesApp {
+  static #$tableHeader = $("<tr>")
+    .append($("<th>").text("Name"))
+    .append($("<th>").text("Rating"))
+    .append($("<th>"));
+
+  constructor() {
+    this.movies = new Map();
+    this.tableMovies = $("#table-movies");
+
+    // Adding event listeners to DOM elements
+    $("#inputs-movie").submit(this.handleSubmit.bind(this));
+    this.tableMovies.on("click", ".btn-remove", this.handleRemove.bind(this));
+  }
+
+  /**
+   * Handles the processing after clicking on the submit button in the form.
+   *
+   * @param {Event} e The click event on the submit button.
+   */
+  handleSubmit(e) {
+    e.preventDefault();
+    this.addMovie($("#input-name").val(), Number($("#input-rating").val()));
+    this.displayMovies();
+  }
+
+  /**
+   * Handles the processing for removal of a movie in the table.
+   *
+   * @param {Event} e The click event on the "remove" button.
+   */
+  handleRemove(e) {
+    this.deleteMovie(e.target.parentElement.firstChild.innerText);
+    e.target.parentElement.remove();
+  }
+
+  /**
+   * Adds a movie to the movie database (Map).
+   *
+   * @param {String} name The name of the movie to add.
+   * @param {Number} rating The rating of the movie to add.
+   */
+  addMovie(name, rating) {
+    this.movies.set(name, rating);
+  }
+
+  /**
+   * Deletes a movie from the movie database (Map).
+   *
+   * @param {String} name The name of the movie to remove.
+   */
+  deleteMovie(name) {
+    this.movies.delete(name);
+  }
+
+  /**
+   * Creates the table rows for each movie in the database and appends it to the movies table.
+   */
+  displayMovies() {
+    this.tableMovies.empty().append(MoviesApp.#$tableHeader);
+
+    for (let [name, rating] of this.movies) {
+      this.tableMovies.append(
+        $("<tr>")
+          .append($("<td>").text(name))
+          .append($("<td>").text(rating))
+          .append(
+            $("<button>")
+              .attr("type", "button")
+              .addClass("btn-remove")
+              .html("&times;")
+          )
+      );
+    }
+  }
+}
